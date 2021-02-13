7ef4654005efe76aa1d1c3cfc3f9c2b255cef343
Kirill Vasilkov <ansecondkirll@gmail.com>
2021-02-13 17:47:55 +0300

    Delete standard components & utils

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <div id="app"></div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component()
+export default class App extends Vue {}
+</script>
 
 <style>
 #app {
